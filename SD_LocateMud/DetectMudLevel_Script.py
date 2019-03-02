f358391c323876a7d8547d7fdcbb275db8c77e69
MOMOKO606 <bianlong0618@qq.com>
2019-03-02 17:56:41 +0800

    tracker function finished.

--- SD_LocateMud/DetectMudLevel_Script.py
+++ SD_LocateMud/DetectMudLevel_Script.py
@@ -1,6 +1,8 @@
 import time
+import math
+import numpy as np
 from openpyxl import Workbook  # 写入excel用的库
-import queue
+
 
 class PsTrackers( list ):
     """
@@ -19,12 +21,17 @@ class PsTrackers( list ):
         m = timewindow * 6 + 1  # 光敏追踪器的行数, 加1是由于队列的结构。
         n = 16
         self.trackers = [[[] for i in range(n)] for j in range(m)]  # 追踪器初值。
-        self.brokelist = [False for i in range(16)]  #  损坏光敏列表。
+        self.brokelist = [False for i in range(16)]  # 损坏光敏列表。
+        self.sum = [0.0 for i in range(16)]  # 追踪器的总值。
+        self.mean = [0.0 for i in range(16)]  # 追踪器的均值。
+        self.stddev = [0.0 for i in range(16)]  # 追踪器的标准差。
+        self.max = [float('-inf') for i in range(16)]  # 追踪器的最大值。
+        self.min = [float('inf') for i in range(16)]  # 追踪器的最小值。
         self.rows = m
         self.head = 0  # 光敏追踪器的head。
         self.tail = 0  # 光敏追踪器的tail。
-        self.mean = 0  # 追踪器的均值。
-        self.stddev = 0  #  追踪器的标准差。
+        self.count = 0  # 表示追踪器内有多少条数据。
+
 
 
     def reset(self):
@@ -35,10 +42,15 @@ class PsTrackers( list ):
         m = self.rows
         self.trackers = [[[] for i in range(n)] for j in range(m)]  # 追踪器初值。
         self.brokelist = [False for i in range(16)]  #  损坏光敏列表。
+        self.mean = [0 for i in range(16)]   # 追踪器的均值。
+        self.sum = [0 for i in range(16)]  # 追踪器的总值。
+        self.stddev = [0 for i in range(16)]   #  追踪器的标准差。
+        self.max = [float('-inf') for i in range(16)]  # 追踪器的最大值。
+        self.min = [float('inf') for i in range(16)]  # 追踪器的最小值。
         self.head = 0  # 光敏追踪器的head。
         self.tail = 0  # 光敏追踪器的tail。
-        self.mean = 0  # 追踪器的均值。
-        self.stddev = 0  #  追踪器的标准差。
+        self.count = 0  #  表示追踪器内有多少条数据。
+
 
 
     def isempty( self ):
@@ -52,6 +64,7 @@ class PsTrackers( list ):
             return False
 
 
+
     def isfull( self ):
         """
         function:判断当前队列是否已满。
@@ -61,6 +74,7 @@ class PsTrackers( list ):
             return True
         else:
             return False
+
 
 
     def dequeue( self ):
@@ -91,12 +105,14 @@ class PsTrackers( list ):
         #  更新属性。
         self.head = head
         self.trackers = trackers
+        return res
+
 
 
     def enqueue(self, psvalues):
         """
         function:对光敏追踪器增加一行数据psvalues。
-        :param psvalues:
+        :param psvalues: 实时光敏数据。
         """
 
         #  取出参数。
@@ -105,42 +121,194 @@ class PsTrackers( list ):
         head = self.head
         tail = self.tail
 
+        #  更新count, sum, mean属性。
         #  如果队列已满，则先从head弹出一行数据。
+        popedvalue = []
         if self.isfull():
-            self.dequeue()
+            popedvalue = self.dequeue()
+            self.count = self.rows - 1
+            self.sum = [self.sum[i]-popedvalue[i] + psvalues[i] for i in range(16)]
+        #  队列未存满时：
+        else:
+            self.count += 1
+            self.sum = [self.sum[i] + psvalues[i] for i in range(16)]
+        self.mean = [self.sum[i] / self.count for i in range(16)]
+
         #  在tail处插入数据。
         trackers[tail] = [psvalues[i] for i in range(16)]
+        self.trackers = trackers  # 更新属性。
         #  修改tail的值。
         if tail == rows - 1:
             tail = 0
         else:
             tail += 1
+        self.tail = tail  # 更新属性。
 
-        #  更新属性。
-        self.tail = tail
-        self.trackers = trackers
+        #  更新stddev。
+        #  遍历追踪器。
+        for j in range(16):
+            tmp = 0  # 累加器，初值为0。
+            for i in range(0, rows):
+                #  Sentinel, 跳过空值。
+                if trackers[i][j] == []:
+                    continue
+                tmp += (trackers[i][j] - self.mean[j]) ** 2
+            tmp /= self.count
+            self.stddev[j] = math.sqrt( tmp )
+
+        return popedvalue  # 返回弹出的一行数据
 
 
-        def Isbroken(self, psvalues):
-            """
-            function: detecting each photosensitive to see whether is good or broken.
-            :param self:
-            :param psvalues:
-            :return:
-            """
 
+    def GetIndices(self):
+        """
+        function: 得到从tail向head方向，遍历的indices.
+        :return: indices数组。
+        """
+        #  读取参数。
+        head = self.head
+        tail = self.tail
+        rows = self.rows
+
+        #  case1.
+        if tail == 0:
+            indices = [rows - 1, 0]
+        #  case2.
+        elif tail > head:
+            indices = [tail - 1, head - 1]
+        #  case3.head > tail
+        else:
+            indices = [[tail - 1, -1], [rows - 1, head - 1]]
+        return indices
+
+
+
+    def Isbroken(self):
+        """
+        function: detecting each photosensitive to see whether is good or broken.
+        """
+        rows = self.rows
+        brokelist = self.brokelist
+        trackers = self.trackers
+        indices = self.GetIndices()
+
+        #  对应GetIndices函数中的case1和case2.
+        if len(np.array(indices).shape) == 1:
+
+            start = indices[0]
+            end = indices[1]
+
+            #  caseA:1分钟内速测(短周期检测)。
+            #  当前值与10s前的值，delta超过380，则光敏损坏。
+            for j in range(16):
+                now = trackers[start][j]
+                previous = trackers[start - 1][j]
+                #  trackers中只存了一个值的情况。
+                if previous == []:
+                    previous = now
+                if abs(now - previous) >= 380:
+                    brokelist[j] = True
+                else:
+                    brokelist[j] = False
+
+            #  caseB:1分钟以上检测(长周期检测)。
+            if rows > 6:
+                for j in range(16):
+                    #  Sentinel，已在短周期被定位为True的不再检测。
+                    if brokelist[j]:
+                        continue
+                    #  标准差大于150,波动较大，疑似随机值：
+                    if self.stddev[j] >= 150:
+                        #  检查时间窗内，追踪器数据是否单调递增或递减。
+                        #  单调递增，则由泥变水；
+                        #  单调递减，则由水变泥。
+                        delta = 0.0
+                        for i in range(start, end + 1, -1):
+                            delta += trackers[i][j] - trackers[i - 1][j]
+                        #  变化值相加后取绝对值，距离900较远说明是随机值，即光敏损坏。
+                        if abs(delta) < 700:
+                            brokelist[j] = True
+                        #  泥位快速升降。
+                        else:
+                            brokelist[j] = False
+
+        #  对应GetIndices函数中的case3.
+        else:
+
+            start1 = indices[0][0]
+            end1 = indices[0][1]
+            start2 = indices[1][0]
+            end2 = indices[1][1]
+
+            #  caseA:1分钟内速测(短周期检测)。
+            #  当前值与10s前的值，delta超过380，则光敏损坏。
+            #  横向检查16个追踪器。
+            for j in range(16):
+                tmplist = []
+                #  纵向从追踪器里找最近的两条数据。
+                #  可优化，只读2条。
+                for i in range(start1,end1,-1):
+                    tmplist.append(trackers[i][j])
+                #  如果已经至少找到2条数据：
+                if (len(tmplist) >= 2):
+                    now = tmplist[0]
+                    previous = tmplist[1]
+                #  只有一条数据:
+                else:
+                    now = tmplist[0]
+                    previous = trackers[start2][j]
+                #  如果10s跳动大于380，则认为是随机值，即光敏损坏。
+                if abs( now - previous) > 380:
+                    brokelist[j] = True
+                else:
+                    brokelist[j] = False
+
+            #  caseB:1分钟以上检测(长周期检测)。
+            if rows > 6:
+                for j in range(16):
+                    #  Sentinel，已在短周期被定位为True的不再检测。
+                    if brokelist[j]:
+                        continue
+                    #  标准差大于150，波动较大，疑似随机值：
+                    if self.stddev[j] >= 150:
+                        #  检查时间窗内，追踪器数据是否单调递增或递减。
+                        #  单调递增，则由泥变水；
+                        #  单调递减，则由水变泥。
+                        delta = 0.0
+                        for i in range(start1,end1 + 1, -1):
+                            delta += trackers[i][j] - trackers[i - 1][j]
+                        for i in range(start2,end2 + 1, -1):
+                            delta += trackers[i][j] - trackers[i - 1][j]
+                        #  变化值相加后取绝对值，距离900较远说明是随机值，即光敏损坏。
+                        if abs(delta) < 700:
+                            brokelist[j] = True
+                        #  泥位快速升降。
+                        else:
+                            brokelist[j] = False
+        #  更新仪器损坏表。
+        self.brokelist = brokelist
+        return brokelist
 
 
 
 def GetMudLvl( inputfile, ws ):
     #  新建并初始化光敏追踪器。
-    timewindow = 1  # 设定追踪器时间窗长，单位为分钟，最小值为1。
+    timewindow = 5  # 设定追踪器时间窗长，单位为分钟，最小值为1，建议选取3-5。
     pst = PsTrackers( timewindow )
-
-    count = 0
-
     with open( inputfile ) as f:
+
+        ###  测试用  ###
+        timecount = 0
+        pre_brokelist = [False for i in range(16)]
+        broke_time = [[] for i in range(16)]
+        ###  测试用  ###
+
         while True:
+
+            ###  测试用  ###
+            timecount += 1
+            ###  测试用  ###
+
             #  读入光敏数据。
             line = f.readline()  # 读取一行光敏数据（str）。
             #  EOF Sentinel.
@@ -151,17 +319,24 @@ def GetMudLvl( inputfile, ws ):
             #  Collecting the photosensitive values.
             pst.enqueue(psvalues)
             #  检查损坏的光敏。
-            pst.Isbroken(psvalues)
+            brokelist = pst.Isbroken()
+
+            ###  测试用  ###
+            for j in range(16):
+                if brokelist[j] != pre_brokelist[j]:
+                    broke_time[j].append(timecount)
+            pre_brokelist = brokelist[:]
+            ###  测试用  ###
 
 
 
-            count += 1
-            if count == 12:
-                break
+
+
         #     #  光敏数据写入excel，用于人工检查。
         #     ws.append(psvalues)  # worksheet中写入一行光敏数据。
         # wb.save(inputfile[:-3] + "xlsx")  # 保存为与输入数据文件同名的excel文件。
     f.close()
+
 
 
 
