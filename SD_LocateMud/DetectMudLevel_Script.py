e8e33dbdb302513bda0b090160c5f1be157ab2d4
MOMOKO606 <bianlong0618@qq.com>
2019-03-05 17:25:09 +0800

    something wrong with the mud inverval function.

--- SD_LocateMud/DetectMudLevel_Script.py
+++ SD_LocateMud/DetectMudLevel_Script.py
@@ -1,7 +1,8 @@
 import time
 import math
 import numpy as np
-from openpyxl import Workbook  # 写入excel用的库
+from openpyxl import Workbook  # 写入excel用的库。
+import openpyxl.styles as sty  # 修改excel颜色。
 
 
 class PsTrackers( list ):
@@ -11,7 +12,7 @@ class PsTrackers( list ):
     def __init__(self, timewindow):
         """
         function: 初始化16个光敏追踪器，即(timewindow*6) * 16的空table, 可视为是16个队列组成的table。
-        :param timewindow: 追踪多长时间的光敏值，单位为分钟，至少1分钟。
+        :param timewindow: 追踪多长时间的光敏值，单位为分钟，至少1分钟，建议3分钟。
         """
 
         #  Sentinel.
@@ -22,15 +23,17 @@ class PsTrackers( list ):
         n = 16
         self.trackers = [[[] for i in range(n)] for j in range(m)]  # 追踪器初值。
         self.brokelist = [False for i in range(16)]  # 损坏光敏列表。
+        self.brokegratings = [False for i in range(16)]  # 光栅损坏的光敏列表。
+        self.gratingcount = [0.0 for i in range(16)]  # 判断光栅是否损坏的辅助数组。
         self.sum = [0.0 for i in range(16)]  # 追踪器的总值。
         self.mean = [0.0 for i in range(16)]  # 追踪器的均值。
         self.stddev = [0.0 for i in range(16)]  # 追踪器的标准差。
-        self.max = [float('-inf') for i in range(16)]  # 追踪器的最大值。
-        self.min = [float('inf') for i in range(16)]  # 追踪器的最小值。
-        self.rows = m
+        # self.max = [float('-inf') for i in range(16)]  # 追踪器的最大值。
+        # self.min = [float('inf') for i in range(16)]  # 追踪器的最小值。
+        self.rows = m  # 由timewindow转换的行数。
         self.head = 0  # 光敏追踪器的head。
         self.tail = 0  # 光敏追踪器的tail。
-        self.count = 0  # 表示追踪器内有多少条数据。
+        self.count = 0  # 表示追踪器内目前存有多少条数据。
 
 
 
@@ -42,11 +45,13 @@ class PsTrackers( list ):
         m = self.rows
         self.trackers = [[[] for i in range(n)] for j in range(m)]  # 追踪器初值。
         self.brokelist = [False for i in range(16)]  #  损坏光敏列表。
+        self.brokegratings = [False for i in range(16)]  # 光栅损坏的光敏列表。
+        self.gratingcount = [0.0 for i in range(16)]  # 判断光栅是否损坏的辅助数组。
         self.mean = [0 for i in range(16)]   # 追踪器的均值。
         self.sum = [0 for i in range(16)]  # 追踪器的总值。
         self.stddev = [0 for i in range(16)]   #  追踪器的标准差。
-        self.max = [float('-inf') for i in range(16)]  # 追踪器的最大值。
-        self.min = [float('inf') for i in range(16)]  # 追踪器的最小值。
+        # self.max = [float('-inf') for i in range(16)]  # 追踪器的最大值。
+        # self.min = [float('inf') for i in range(16)]  # 追踪器的最小值。
         self.head = 0  # 光敏追踪器的head。
         self.tail = 0  # 光敏追踪器的tail。
         self.count = 0  #  表示追踪器内有多少条数据。
@@ -96,6 +101,22 @@ class PsTrackers( list ):
         res = [trackers[head][j] for j in range(16)]
         trackers[head] = [[] for j in range(16)]
 
+        #  检测弹出的数据是否包含光栅损坏的光敏。
+        for j in range(16):
+            if j == 0:  # 左边界情况。
+                left = True
+                right = (res[j + 1] - res[j]) > 150
+            elif j == 15:  # 右边界情况。
+                right = True
+                left = (res[j - 1] - res[j]) > 150
+            else:
+                left = (res[j - 1] - res[j]) > 150
+                right = (res[j + 1] - res[j]) > 150
+            #  当角标为j的光敏值小于左右两边光敏值时,疑似光栅损坏；
+            #  因为是弹出该数据，所以对应gratingcount减1。
+            if left and right:
+                self.gratingcount[j] -= 1
+
         #  修改head的位置。
         if head == rows - 1:
             head = 0
@@ -144,6 +165,27 @@ class PsTrackers( list ):
             tail += 1
         self.tail = tail  # 更新属性。
 
+        #  检测插入的数据是否包含光栅损坏的光敏。
+        for j in range(16):
+            if j == 0:  # 左边界情况。
+                left = True
+                right = (psvalues[j + 1] - psvalues[j]) > 150
+            elif j == 15:  # 右边界情况。
+                right = True
+                left = (psvalues[j - 1] - psvalues[j]) > 150
+            else:
+                left = (psvalues[j - 1] - psvalues[j]) > 150
+                right = (psvalues[j + 1] - psvalues[j]) > 150
+            #  当角标为j的光敏值小于左右两边光敏值时,疑似光栅损坏；
+            #  因为是插入该数据，所以对应gratingcount加1
+            if left and right:
+                self.gratingcount[j] += 1
+                #  在历史数据中，光栅疑似故障的比例大于等于83%，则认为光栅损坏。
+                if self.gratingcount[j] / self.count >= 5/6:
+                    self.brokegratings[j] = True
+                else:
+                    self.brokegratings[j] = False
+
         #  更新stddev。
         #  遍历追踪器。
         for j in range(16):
@@ -162,7 +204,7 @@ class PsTrackers( list ):
 
     def GetIndices(self):
         """
-        function: 得到从tail向head方向，遍历的indices.
+        function: 得到从tail向head方向的角标indices.
         :return: indices数组。
         """
         #  读取参数。
@@ -170,13 +212,13 @@ class PsTrackers( list ):
         tail = self.tail
         rows = self.rows
 
-        #  case1.
+        #  case1.queue第一次插满后，又插入一条数据，tail回到0角标的情况。
         if tail == 0:
-            indices = [rows - 1, 0]
-        #  case2.
+            indices = [rows - 1, -1]
+        #  case2.queue不满时。
         elif tail > head:
             indices = [tail - 1, head - 1]
-        #  case3.head > tail
+        #  case3.head > tail，除case1外，queue插满时还在继续插入数据的情况。
         else:
             indices = [[tail - 1, -1], [rows - 1, head - 1]]
         return indices
@@ -186,17 +228,36 @@ class PsTrackers( list ):
     def Isbroken(self):
         """
         function: detecting each photosensitive to see whether is good or broken.
+
+        光敏完全损坏：
+        检查1.检测当前光敏值与10s前光敏值的差值，大于380认为是随机值，即光敏损坏。
+        检查2.检查追踪器中的标准差。如果光敏损坏，会产生0到1000的随机值，其随机分布标准差在280左右。
+             所以当追踪器中的标准差接近280时，说明该光敏值是随机的，即光敏损坏。
+             但是，如果泥位迅速增高或下降时，也有可能产生0，200，400，600，... , 900的情况。
+             这种情况的标准差较大，但其两两光敏值的差值是同一方向的，也就是说delta差值相加应该在900或-900左右。
+             所以通过delta值相加来判断是否属于该情况。
+
+        光敏中有光栅损坏：
+        检测是否有值总低于左右两边的光敏值。
+
+        return: 完全损坏的光敏列表brokelist，有光栅损坏的光敏列表brokegratings.
         """
+
+        #  载入数据。
         rows = self.rows
         brokelist = self.brokelist
+        brokegratings = self.brokegratings
         trackers = self.trackers
-        indices = self.GetIndices()
 
+        #  首先要知道从近到远的历史数据顺序，
+        #  即通过GetIndices函数得到从tail到head的角标。
+        indices = self.GetIndices()
         #  对应GetIndices函数中的case1和case2.
         if len(np.array(indices).shape) == 1:
-
             start = indices[0]
             end = indices[1]
+
+            #  检查光敏是否完全损坏。
 
             #  caseA:1分钟内速测(短周期检测)。
             #  当前值与10s前的值，delta超过380，则光敏损坏。
@@ -223,7 +284,7 @@ class PsTrackers( list ):
                         #  单调递增，则由泥变水；
                         #  单调递减，则由水变泥。
                         delta = 0.0
-                        for i in range(start, end + 1, -1):
+                        for i in range(start, end, -1):
                             delta += trackers[i][j] - trackers[i - 1][j]
                         #  变化值相加后取绝对值，距离900较远说明是随机值，即光敏损坏。
                         if abs(delta) < 700:
@@ -231,7 +292,6 @@ class PsTrackers( list ):
                         #  泥位快速升降。
                         else:
                             brokelist[j] = False
-
         #  对应GetIndices函数中的case3.
         else:
 
@@ -291,24 +351,65 @@ class PsTrackers( list ):
 
 
 
+def FuzzyInterval(brokelist, brokegratings, psvalues):
+    """
+    function:根据当前的16个光敏值判断泥面位于的区间。
+    :param brokelist: 当前损坏的光敏列表。
+    :param brokegratings: 光栅存在问题的光敏列表。
+    :param psvalues: 当前的光敏值。
+    :return: 泥位位于的光敏区间。
+    """
+
+    #  从最左端开始搜索泥位区间的左边界。
+    j = 0
+    while True:
+        #  sentinel, j光敏损坏。
+        if brokelist[j]:
+            l = j + 1
+            continue
+
+        #  sentinel, j光栅损坏。
+        if brokegratings[j]:
+            l = j
+            continue
+
+        if abs(psvalues[j + 1] - psvalues[j]) >= 200:
+            l = j
+            break
+        j += 1
+
+    #  从最右端开始搜索泥位区间的右边界。
+    j = 15
+    while True:
+        #  sentinel, j光敏损坏。
+        if brokelist[j]:
+            r = j - 1
+            continue
+
+        #  sentinel, j光栅损坏。
+        if brokegratings[j]:
+            r = j
+            continue
+
+        if abs(psvalues[j - 1] - psvalues[j]) >= 200:
+            r = j
+            break
+        j -= 1
+    return [l, r]
+
+
 def GetMudLvl( inputfile, ws ):
     #  新建并初始化光敏追踪器。
-    timewindow = 5  # 设定追踪器时间窗长，单位为分钟，最小值为1，建议选取3-5。
+    timewindow = 3  # 设定追踪器时间窗长，单位为分钟，最小值为1，建议选取3-5。
     pst = PsTrackers( timewindow )
     with open( inputfile ) as f:
-
         ###  测试用  ###
         timecount = 0
-        pre_brokelist = [False for i in range(16)]
-        broke_time = [[] for i in range(16)]
         ###  测试用  ###
-
         while True:
-
             ###  测试用  ###
             timecount += 1
             ###  测试用  ###
-
             #  读入光敏数据。
             line = f.readline()  # 读取一行光敏数据（str）。
             #  EOF Sentinel.
@@ -318,29 +419,27 @@ def GetMudLvl( inputfile, ws ):
             psvalues = [float(value) for value in line]  # 将str转换为float，PhotoSensitive Value。
             #  Collecting the photosensitive values.
             pst.enqueue(psvalues)
-            #  检查损坏的光敏。
+            #  纵向检测：检查损坏的光敏。
             brokelist = pst.Isbroken()
-
-            ###  测试用  ###
+            brokegratings = pst.brokegratings
+            #  横向检测：计算泥位区间。
+            mudinterval = FuzzyInterval(brokelist, brokegratings, psvalues)
+            #  光敏数据写入excel，用于人工检查。
+            ws.append(psvalues)  # worksheet中写入一行光敏数据。
             for j in range(16):
-                if brokelist[j] != pre_brokelist[j]:
-                    broke_time[j].append(timecount)
-            pre_brokelist = brokelist[:]
-            ###  测试用  ###
-
-
-
-
-
-        #     #  光敏数据写入excel，用于人工检查。
-        #     ws.append(psvalues)  # worksheet中写入一行光敏数据。
-        # wb.save(inputfile[:-3] + "xlsx")  # 保存为与输入数据文件同名的excel文件。
-    f.close()
-
-
-
-
-
+                #  将光栅损坏的光敏标记成绿色。
+                if brokegratings[j]:
+                    ws.cell(row = timecount, column = j + 1).fill = \
+                        sty.PatternFill(fill_type = "solid", fgColor = "0000FF00")
+                #  将完全损坏的光敏标记成紫色。
+                if brokelist[j]:
+                    ws.cell(row = timecount, column = j + 1).fill = \
+                        sty.PatternFill(fill_type = "solid", fgColor = "007A378B")
+            #  将泥位区间标记为红色。
+            for i in mudinterval:
+                ws.cell(row = timecount, column = i + 1).fill = \
+                    sty.PatternFill(fill_type = "solid", fgColor = "00FF0000")
+        wb.save(inputfile[:-3] + "xlsx")  # 保存为与输入数据文件同名的excel文件。
 
 
 
@@ -351,7 +450,12 @@ Step0.预处理光敏数据：
       选出一个池子，某段时间内的16组光敏数据，保存为txt文档，默认名：PreData02.txt。
       e.g. 选用db_181013/sanchang_db数据中的5号池，共262943个数（大约1个月）。
 
-Step1.
+Step1.定义光敏追踪器类，纵向追踪历史数据。
+Step2.首先通过光敏追踪器过滤掉当前时刻的问题光敏，问题光敏有2种情况：
+      （1）光敏彻底坏掉，跳随机值。
+      （2）光敏中的个别光栅坏掉，其数值总比正常值低200+。
+      再横向计算污泥区间。
+Step3.跟踪污泥区间，连续的污泥区间应该是连续的。
 """
 
 start = time.time()  # 程序开始时间。
